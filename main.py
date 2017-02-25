d1ebd5c53aa5a5a38fed8cb5e5a7b9f2436d012b
Kerlin Michel <michelkerlin@gmail.com>
2017-02-25 13:22:43 -0500

    Simple python server

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/rec_img")
+def rec_img():
+  file = request.files['img']
+  file.save(os.path.join('./', filename))
+  return "img"
+
+if __name__ == "__main__":
+  app.run
