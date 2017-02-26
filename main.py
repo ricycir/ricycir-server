0a57466c4269a6fbf298f88db65e580ac6aad6fd
root <root@kerlin-dev.localdomain>
2017-02-26 01:35:51 +0000

    fixed bug with thread by using python 3 threads

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ from PIL import Image
 from array import array
 import socket
 import sys
-from thread import *
-
+#from thread import *
+import _thread
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('0.0.0.0', 5001)
 sock.bind(server_address)
 sock.listen(1)
 
 new_data = False
-
+print('print test')
 def client_thread(conn):
   while True:
     data = conn.recv(1024)
-      if not data:
-        break
-      if new_data:
-        conn.sendall('Test')
-        new_data = False
+    if not data:
+      break
+    if new_data:
+      print('socket msg')
+      conn.sendall('Test')
+      new_data = False
   # out of loop so close connection
   conn.close()
 
@@ -41,4 +42,4 @@ if __name__ == "__main__":
   while True:
     connection = sock.accept()
     new_data = True
-    start_new_thread(client_thread, (connection,))
+    _thread.start_new_thread(client_thread, (connection,))
