e5427401c44187bfb5d40ee7f4fc6c41258ee623
root <root@kerlin-dev.localdomain>
2017-02-26 01:23:02 +0000

    Added socket server

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python:3-onbuild
+
 CMD [ "python", "./main.py" ]
 
 EXPOSE 5000
+EXPOSE 5001
--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 from flask import Flask, request
 import os
+import io
+from PIL import Image
+from array import array
+import socket
+import sys
+from thread import *
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('0.0.0.0', 5001)
+sock.bind(server_address)
+sock.listen(1)
+
+new_data = False
+
+def client_thread(conn):
+  while True:
+    data = conn.recv(1024)
+      if not data:
+        break
+      if new_data:
+        conn.sendall('Test')
+        new_data = False
+  # out of loop so close connection
+  conn.close()
 
 app = Flask(__name__)
 
-@app.route("/rec_img")
+@app.route("/rec_img", methods=['POST'])
 def rec_img():
-  file = request.files['img']
-  file.save(os.path.join('./', filename))
-  print("HIT")
+  file = request.files['uploaded']
+  bytes = bytearray(file.read())
+  image = Image.open(io.BytesIO(bytes))
+  image.save("./test.bmp")
   return "recieved image"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000)
+  
+  while True:
+    connection = sock.accept()
+    new_data = True
+    start_new_thread(client_thread, (connection,))
